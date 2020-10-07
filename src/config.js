fd89e515f45848eea06032d1bdcfbf00268c4ade
Nicholas Gibson <nckgibson4@gmail.com>
2020-10-07 11:29:45 -0700

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "nick-gibson-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://vszjvh1vvk.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_ZDNcw0NYy",
+      APP_CLIENT_ID: "6hn50fckj7agcbktmkt27e9o6k",
+      IDENTITY_POOL_ID: "us-east-1:4f4e07a0-3ff6-4164-b281-f34848bfbb3f"
+    }
+  };
